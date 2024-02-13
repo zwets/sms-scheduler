f0a9ee3d12e17a2a8cf3a95467c15c3635c443c4
Marco van Zwetselaar <io@zwets.it>
2024-02-13 16:01:50 +0300

    Release 0.9.7

--- bin/run-scheduler-test.sh
+++ bin/run-scheduler-test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 cd "$(dirname "$(realpath "$0")")/.."
 
-RELEASE=0.9.1-SNAPSHOT
+RELEASE=0.9.7
 JAR="target/sms-scheduler-${RELEASE}.jar"
 
 exec java -jar "$JAR" --spring.profiles.active='test' "$@"
