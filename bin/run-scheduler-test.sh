3154fb59212518b2da0b89f4db41b966f36deb63
Marco van Zwetselaar <io@zwets.it>
2024-02-15 15:34:15 +0300

    Start developing on 1.0.1-SNAPSHOT

--- bin/run-scheduler-test.sh
+++ bin/run-scheduler-test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 cd "$(dirname "$(realpath "$0")")/.."
 
-VERSION=1.0.0
+VERSION=1.0.1-SNAPSHOT
 JAR="target/sms-scheduler-${VERSION}.war"
 
 exec java -jar "$JAR" --spring.profiles.active='test' "$@"
