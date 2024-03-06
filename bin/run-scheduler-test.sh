8ca477a687e82cffee39bed8ee7a94435259f871
Marco van Zwetselaar <io@zwets.it>
2024-03-06 11:20:11 +0300

    Make run-scheduler-test.sh work like the systemd service

--- bin/run-scheduler-test.sh
+++ bin/run-scheduler-test.sh
@@ -5,7 +5,6 @@ set -euo pipefail
 
 cd "$(dirname "$(realpath "$0")")/.."
 
-VERSION=1.0.6-SNAPSHOT
-JAR="target/sms-scheduler-${VERSION}.war"
-
+# This assumes a symlink in our base directory to the versioned war
+JAR="sms-scheduler.jar"
 exec java -jar "$JAR" --spring.profiles.active='test' "$@"
